573bda2e71607aacc0f17cccc48f6f5b217e2b0e
KPaulina <41211513+KPaulina@users.noreply.github.com>
2023-01-02 22:47:15 +0100

    fix: fixing where the json file gets once it is scraped

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 from extract import json_scraper
 from datetime import datetime
 from load import load_data
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))[:-3]
+DATA_DIR = os.path.join(BASE_DIR, 'data')
 
 time = str(datetime.now())[:10]
 
 if __name__ == "__main__":
-    json_scraper('https://open.er-api.com/v6/latest/PLN', f"exchange_rate_PLN_{time}.json")
+    json_scraper('https://open.er-api.com/v6/latest/PLN', os.path.join(DATA_DIR, f'exchange_rate_PLN_{time}.json'))
     load_data()
